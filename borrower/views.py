9f818ba3af82cebac7a561a18873f11303ad1ade
unknown <eske17.ea@gmail.com>
2023-02-02 11:29:30 +0300

    change structure:

--- borrower/views.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, HTTPException
-from database import engine
-from sqlmodel import Session, select
-from .models import Borrower
-from starlette.responses import JSONResponse
-from starlette.status import HTTP_201_CREATED, HTTP_409_CONFLICT
-from repos.user_repos import select_all_users, select_user, find_user
-from auth.auth import AuthHandler
